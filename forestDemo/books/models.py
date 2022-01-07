dbb5e015368406e18b4a382088ffa14092590563
Valentin Monté <valentin.monte@yahoo.fr>
2022-01-07 15:32:06 +0100

    feat: Add the books models

---
+++ forestDemo/books/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.deletion import SET, SET_NULL
+
+
+class Book(models.Model):
+    author = models.ForeignKey(
+        "Author", on_delete=SET_NULL, related_name="books", null=True
+    )
+    name = models.CharField(max_length=254, blank=False, null=False)
+    categories = models.ManyToManyField("Category", related_name="books")
+    release_date = models.DateField(auto_now=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    active = models.BooleanField(default=False)
+    isbn = models.CharField(max_length=13)
+    created_by = models.ForeignKey(
+        User, on_delete=SET_NULL, related_name="created_books", null=True
+    )
+
+
+class Author(models.Model):
+    MALE = "M"
+    FEMALE = "F"
+
+    GENDERS = ((MALE, "MALE"), (FEMALE, "FEMALE"))
+
+    gender = models.CharField(max_length=1, choices=GENDERS)
+    firstname = models.CharField(max_length=254, blank=False, null=False)
+    lastname = models.CharField(max_length=254, blank=False, null=False)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254, blank=False, null=False)
